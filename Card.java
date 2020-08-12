eb81279f798d5134d5b57635bfada7eaefb97989
RyanGutrz <66034540+RyanGutrz@users.noreply.github.com>
2020-08-11 22:15:23 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -9,6 +9,16 @@ package hearts_ryanc;
  *
  * @author ryanc
  */
-public class Card {
+public abstract class Card 
+{
+    //default modifier for child classes
+    
+    /**
+     * Students should implement this method for their specific children classes 
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    
+    @Override
+    public abstract String toString();
     
 }

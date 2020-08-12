7d8bb3f6993c92ce247b05f2dd89b26727d37d7f
RyanGutrz <66034540+RyanGutrz@users.noreply.github.com>
2020-08-11 23:17:58 -0400

    Update Score.java

--- Score.java
+++ Score.java
@@ -10,5 +10,12 @@ package hearts_ryanc;
  * @author ryanc
  */
 public class Score {
-    
-}
+      
+  private Integer[] score = new Integer[4];
+
+  private int roundScore() {
+    for (int player = 0; player < 4; player++) {
+      score[player] = 0;
+    }
+  }
+
